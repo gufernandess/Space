961b33347f987a29aad306e34c343d5624e6e091
gufernandess <gustavofer14@alu.ufc.br>
2022-01-13 14:26:24 -0300

    Ajustes de responsividade

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,15 @@
 const menu = document.querySelector("header img");
 const nav = document.querySelector("nav");
+const page = document.querySelector("body");
 
 menu.addEventListener("click", () => {
     var navState = window.getComputedStyle(nav).display;
 
     if(navState == "none") {
         nav.style.display = "flex";
+        page.style.overflow = "hidden";
     } else {
         nav.style.display = "none";
+        page.style.overflow = "inherit";
     }
 });
